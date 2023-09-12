a02e3d2f78a7990cf701d80a632862e87ed7a7e6
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-12 16:19:32 +0530

    human friendly random paths

---
+++ internal/random_strings.go
@@ -0,0 +1,40 @@
+package internal
+
+import (
+	"math/rand"
+	"strings"
+)
+
+var randomWords = []string{
+	"humpty",
+	"dumpty",
+	"Horsey",
+	"donkey",
+	"yikes",
+	"monkey",
+	"Coo",
+	"scooby",
+	"dooby",
+	"vanilla",
+	"237",
+	"Monkey",
+}
+
+func randomWord() string {
+	return randomWords[rand.Intn(len(randomWords))]
+}
+
+func randomString(n int, joiner string) string {
+	b := make([]string, n)
+
+	for i := range b {
+		b[i] = randomWord()
+	}
+
+	return strings.Join(b, joiner)
+}
+
+func randomUrl() string {
+	size := rand.Intn(2) + 1
+	return randomString(size, "-")
+}
