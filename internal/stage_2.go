bbddf4d7e72c22d079239212f50bc0d67d9b4452
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-12 16:33:41 +0530

    Move common stuff out of stage specific code

--- internal/stage_2.go
+++ internal/stage_2.go
@@ -1,10 +1,6 @@
 package internal
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
-
 	testerutils "github.com/codecrafters-io/tester-utils"
 )
 
@@ -19,32 +15,4 @@ func test200OK(stageHarness *testerutils.StageHarness) error {
 	httpClient := NewHTTPClient()
 
 	return requestWithStatus(httpClient, URL, 200, logger)
-}
-
-func requestWithStatus(client *http.Client, url string, statusCode int, logger *testerutils.Logger) error {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return err
-	}
-	reqDump, err := httputil.DumpRequestOut(req, true)
-	if err != nil {
-		return err
-	}
-	logger.Debugf("Sending Request:\n%s", string(reqDump))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		logFriendlyError(logger, err)
-		return fmt.Errorf("Failed to connect to server, err: '%v'", err)
-	}
-	respDump, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		return err
-	}
-	logger.Debugf("Received Response:\n%s", string(respDump))
-
-	if resp.StatusCode != statusCode {
-		return fmt.Errorf("Expected status code %d, got %d", statusCode, resp.StatusCode)
-	}
-	return nil
 }
--- internal/stage_common.go
+++ internal/stage_common.go
@@ -1,7 +1,12 @@
 package internal
 
 import (
+	"fmt"
 	"math/rand"
+	"net/http"
+	"net/http/httputil"
+
+	testerutils "github.com/codecrafters-io/tester-utils"
 )
 
 const URL = "http://localhost:4221/"
@@ -17,4 +22,40 @@ func randSeq(n int) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+func sendRequest(client *http.Client, url string, logger *testerutils.Logger) (*http.Response, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	reqDump, err := httputil.DumpRequestOut(req, true)
+	if err != nil {
+		return nil, err
+	}
+	logger.Debugf("Sending Request:\n%s", string(reqDump))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		logFriendlyError(logger, err)
+		return nil, fmt.Errorf("Failed to connect to server, err: '%v'", err)
+	}
+	respDump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		return nil, err
+	}
+	logger.Debugf("Received Response:\n%s", string(respDump))
+	return resp, nil
+}
+
+func requestWithStatus(client *http.Client, url string, statusCode int, logger *testerutils.Logger) error {
+	resp, err := sendRequest(client, url, logger)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != statusCode {
+		return fmt.Errorf("Expected status code %d, got %d", statusCode, resp.StatusCode)
+	}
+	return nil
 }
