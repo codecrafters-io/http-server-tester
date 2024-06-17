3e97e7df638523b806ea79b78e638c091ced943b
Ryan Gang <ryan-gg@outlook.com>
2024-06-17 14:25:19 +0530

    feat: add test to check if server closes down once all connections are closed

--- internal/stage_6.go
+++ internal/stage_6.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/codecrafters-io/tester-utils/logger"
+
 	http_assertions "github.com/codecrafters-io/http-server-tester/internal/http/assertions"
 	http_connection "github.com/codecrafters-io/http-server-tester/internal/http/connection"
 	http_parser "github.com/codecrafters-io/http-server-tester/internal/http/parser"
@@ -19,39 +21,50 @@ func testHandlesMultipleConcurrentConnections(stageHarness *test_case_harness.Te
 	}
 
 	logger := stageHarness.Logger
-	connectionCount := random.RandomInt(2, 5)
-	logger.Infof("Creating %d parallel connections", connectionCount)
-
-	conns := make([]*http_connection.HttpConnection, connectionCount)
-
-	for i := 0; i < connectionCount; i++ {
-		logger.Debugf("Creating connection %d", i+1)
-		conn, err := http_connection.NewInstrumentedHttpConnection(stageHarness, TCP_DEST, fmt.Sprintf("client-%d", i+1))
-		if err != nil {
-			logFriendlyError(logger, err)
-			return err
-		}
-		conns[i] = conn
-	}
+	connectionCount := random.RandomInt(2, 3)
 
 	request, _ := http.NewRequest("GET", URL, nil)
 	expectedStatusLine := http_parser.StatusLine{Version: "HTTP/1.1", StatusCode: 200, Reason: "OK"}
 	expectedResponse := http_parser.HTTPResponse{StatusLine: expectedStatusLine}
-	test_case := test_cases.SendRequestTestCase{
+	testCase := test_cases.SendRequestTestCase{
 		Request:                   request,
 		Assertion:                 http_assertions.NewHTTPResponseAssertion(expectedResponse),
 		ShouldSkipUnreadDataCheck: false,
 	}
 
+	connections, err := spawnConnections(stageHarness, connectionCount, logger)
+	if err != nil {
+		return err
+	}
+	logger.Debugf("Sending first set of requests")
 	for i := 0; i < connectionCount; i++ {
-		if err := test_case.RunWithConn(conns[i], logger); err != nil {
+		if err := testCase.RunWithConn(connections[i], logger); err != nil {
+			return err
+		}
+
+		logger.Debugf("Closing connection %d", i+1)
+		err := connections[i].Close()
+		if err != nil {
+			logFriendlyError(logger, err)
 			return err
 		}
 	}
 
+	// At this point, we have closed all open connections.
+	// But the server should still be running.
+	// We will now spawn new connections and send requests again.
+	connections, err = spawnConnections(stageHarness, connectionCount, logger)
+	if err != nil {
+		return err
+	}
+	logger.Debugf("Sending second set of requests")
 	for i := 0; i < connectionCount; i++ {
+		if err := testCase.RunWithConn(connections[i], logger); err != nil {
+			return err
+		}
+
 		logger.Debugf("Closing connection %d", i+1)
-		err := conns[i].Close()
+		err := connections[i].Close()
 		if err != nil {
 			logFriendlyError(logger, err)
 			return err
@@ -59,4 +72,20 @@ func testHandlesMultipleConcurrentConnections(stageHarness *test_case_harness.Te
 	}
 
 	return nil
+}
+
+func spawnConnections(stageHarness *test_case_harness.TestCaseHarness, connectionCount int, logger *logger.Logger) ([]*http_connection.HttpConnection, error) {
+	logger.Infof("Creating %d parallel connections", connectionCount)
+	connections := make([]*http_connection.HttpConnection, connectionCount)
+
+	for i := 0; i < connectionCount; i++ {
+		logger.Debugf("Creating connection %d", i+1)
+		conn, err := http_connection.NewInstrumentedHttpConnection(stageHarness, TCP_DEST, fmt.Sprintf("client-%d", i+1))
+		if err != nil {
+			logFriendlyError(logger, err)
+			return nil, err
+		}
+		connections[i] = conn
+	}
+	return connections, nil
 }
