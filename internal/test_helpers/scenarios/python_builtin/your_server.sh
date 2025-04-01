9400eadc70f3e9c7531ed27310a6986a16faae97
Ryan Gang <ryan-gg@outlook.com>
2025-04-01 20:28:01 +0530

    feat: add initial setup for Python HTTP server challenge with configuration and README

---
+++ internal/test_helpers/scenarios/python_builtin/your_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# DON'T EDIT THIS!
+#
+# CodeCrafters uses this file to test your code. Don't make any changes here!
+#
+# DON'T EDIT THIS!
+cd $(dirname $0)
+exec python3 -m http.server 4221 --protocol HTTP/1.1
