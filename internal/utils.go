f65e103552fcd0e061cc3109cfa4b06ecaeaf434
Ryan Gang <ryan-gg@outlook.com>
2025-04-03 13:31:39 +0530

    feat: add function to spawn multiple persistent HTTP connections with enhanced logging

--- internal/utils.go
+++ internal/utils.go
@@ -37,6 +37,35 @@ func spawnPersistentConnection(stageHarness *test_case_harness.TestCaseHarness, 
 	return conn, nil
 }
 
+func spawnPersistentConnections(stageHarness *test_case_harness.TestCaseHarness, connectionCount int, logger *logger.Logger) ([]*http_connection.HttpConnection, error) {
+	logger.Debugf("Creating %d persistent connections", connectionCount)
+	connections := make([]*http_connection.HttpConnection, connectionCount)
+
+	for i := 0; i < connectionCount; i++ {
+		conn, err := http_connection.NewInstrumentedHttpConnection(stageHarness, TCP_DEST, fmt.Sprintf("client-%d", i+1))
+		if err != nil {
+			logFriendlyError(logger, err)
+			return nil, err
+		}
+
+		// We want to log all the requests at once, not one by one
+		// Note: No support for logPrefix here
+		conn.Callbacks.BeforeSendRequest = nil
+		conn.Callbacks.AfterReadResponse = func(response http_parser.HTTPResponse) {
+			for _, line := range strings.Split(strings.TrimSpace(response.FormattedString()), "\r\n") {
+				stageHarness.Logger.Debugf("%s%s", "< ", line)
+			}
+			stageHarness.Logger.Debugf("%s%s", "< ", "")
+			// TODO: Update conn count + host addr
+			stageHarness.Logger.Debugf("* Connection #%d to host localhost left intact", i+1)
+		}
+
+		connections[i] = conn
+	}
+
+	return connections, nil
+}
+
 func spawnConnections(stageHarness *test_case_harness.TestCaseHarness, connectionCount int, logger *logger.Logger) ([]*http_connection.HttpConnection, error) {
 	logger.Infof("Creating %d parallel connections", connectionCount)
 	connections := make([]*http_connection.HttpConnection, connectionCount)
