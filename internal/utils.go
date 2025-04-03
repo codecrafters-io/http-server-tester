5825ee1fa77d26bd88c84f6f0e4ae01b88b15b03
Ryan Gang <ryan-gg@outlook.com>
2025-04-03 12:11:44 +0530

    feat: add functions to manage HTTP connections, including spawning and status checking

--- internal/utils.go
+++ internal/utils.go
@@ -9,6 +9,18 @@ import (
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
+func spawnConnection(stageHarness *test_case_harness.TestCaseHarness, logger *logger.Logger) (*http_connection.HttpConnection, error) {
+	logger.Debugf("Creating connection")
+
+	conn, err := http_connection.NewInstrumentedHttpConnection(stageHarness, TCP_DEST, "")
+	if err != nil {
+		logFriendlyError(logger, err)
+		return nil, err
+	}
+
+	return conn, nil
+}
+
 func spawnConnections(stageHarness *test_case_harness.TestCaseHarness, connectionCount int, logger *logger.Logger) ([]*http_connection.HttpConnection, error) {
 	logger.Infof("Creating %d parallel connections", connectionCount)
 	connections := make([]*http_connection.HttpConnection, connectionCount)
@@ -41,4 +53,11 @@ func repeatSingleConnection(stageHarness *test_case_harness.TestCaseHarness, con
 	}
 
 	return connections, nil
+}
+
+func assertConnectionIsOpen(connection *http_connection.HttpConnection, logger *logger.Logger) error {
+	if !connection.IsOpen() {
+		return fmt.Errorf("Expected connection to stay open, but it is closed")
+	}
+	return nil
 }
